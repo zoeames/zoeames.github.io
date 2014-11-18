cc04f02501376469973cfd675262ca117963e1b1
Zoe Ames <zoeames@gmail.com>
2014-11-17 21:56:47 -0600

    profile pic changed, particles.js added to astrophysics page

--- views/astrophysics/astrophysics.js
+++ views/astrophysics/astrophysics.js
@@ -3,7 +3,40 @@
 
   angular.module('zoeames')
   .controller('AstroCtrl', ['$scope', function($scope){
-
+    particlesJS('particles-js', {
+    particles: {
+        color: '#FBEA74',
+        shape: 'circle',
+        opacity: 1,
+        size: 2.5,
+        size_random: true,
+        nb: 100,
+        line_linked: {
+            enable_auto: true,
+            distance: 250,
+            color: '#FBEA74',
+            opacity: 0.5,
+            width: 1,
+            condensed_mode: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 600
+            }
+        },
+        anim: {
+            enable: true,
+            speed: 2
+        }
+    },
+    interactivity: {
+        enable: true,
+        mouse: {
+            distance: 200
+        },
+        mode: 'grab'
+    },
+    retina_detect: true
+    });
 
   }]);
 })();
